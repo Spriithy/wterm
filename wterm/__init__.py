9253296db716605d782b3ac25b4701e648a381bc
Spriithy <theo.dano@gmail.com>
2020-08-19 12:15:52 +0200

    v0.0.4

--- wterm/__init__.py
+++ wterm/__init__.py
@@ -1,11 +1,13 @@
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from .console import Console
+from .logging import Logger, FileLogger
 
 console = Console()
 
 __all__ = [
     'console',
     'Console',
-    'logging',
+    'Logger',
+    'FileLogger',
 ]
